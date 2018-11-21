cf6f85db2f034075d671a0508f5a947ef2cf34c4
maxwellchung <43402517+maxwellchung@users.noreply.github.com>
2018-11-21 08:51:16 +0100

    new08

--- sketch.js
+++ sketch.js
@@ -1,11 +1,35 @@
-function preload(){
-  // put preload code here
-}
+var value = 0;
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  setMoveThreshold(5);
 }
 
 function draw() {
-  // put drawing code here
+  background(0);
+  fill(255-value*10);
+  textAlign(CENTER);
+  textSize(50);
+  text('Wiggle!',width/2,450);
+   for(var j=0;j<=width/250+15;j++){
+	for(var i=0;i<=height/50+45;i++){
+
+      var r = random(250);
+      if (r<2) {
+        fill(value,value,r*50,450);
+      } else {
+        stroke(r*10,value,value,350);
+      }
+      rect(j*500,400*j,value);
+    }
+  }
+
+  if(value>650){
+    value=random(120);
+  }
+
+}
+
+function deviceMoved() {
+  value++;
 }
